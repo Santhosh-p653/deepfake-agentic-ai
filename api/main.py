d05d5c760eaf0ccaae042ce86e84f604ff2fb1eb
Santhosh-p653 <santhosh11042007@gmail.com>
2026-02-09 17:41:51 +0000

    Added logging enpoints

--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-
+import logging
+logging.basicConfig(level=logging.INFO)
+logger=logging.getLogger(__name__)
 app=FastAPI()
 @app.get("/")
 def health():
+	logger.info("Health endpoint called")
 	return {"status":"ok"}
+	
