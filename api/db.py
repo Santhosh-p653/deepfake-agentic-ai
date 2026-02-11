7cc181be3b277c648479e5b100f8aed65573b54a
Santhosh-p653 <santhosh11042007@gmail.com>
2026-02-11 23:17:57 +0530

    Refactor database connection check function
    Refactor check_db_connection function for better readability and error handling.

--- api/db.py
+++ api/db.py
@@ -1,18 +1,17 @@
-from sqlalchemy import create_engine
-from sqlalchemy.exc import SQLAlchemyError
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+
 load_dotenv()
-DATABASE_URL=os.getenv("DATABASE_URL")
-engine=create_engine(DATABASE_URL)
-def check_db_connection:
-	try:
-		with engine.connect() as connection:
-		connection.execute(text("SELECT 1"))
-		return True
-		print("Database Connection Success")
-		connection.close()
-	except SQLAlchemyError as e:
-		print("Database Connection Failed")
-		print(str(e))
-		return False
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+def check_db_connection():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+            return True
+    except SQLAlchemyError:
+        return False
