2c1170dc7973f40d10eb2c2bd6a3437664772b42
Santhosh-p653 <santhosh11042007@gmail.com>
2026-02-11 17:29:10 +0000

    Updated main function to check  db connection and added langgraph as dependency in api section

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+fromdb import check_db_connection
 import logging
 logging.basicConfig(level=logging.INFO)
 logger=logging.getLogger(__name__)
@@ -6,5 +7,9 @@ app=FastAPI()
 @app.get("/")
 def health():
 	logger.info("Health endpoint called")
-	return {"status":"ok"}
-	
+	db_status=check_db_connection()
+	if db_status:
+		return {"status":"ok","database":"connected"}
+	else:
+		return {"status":"error","database":"disconnected"}
+
