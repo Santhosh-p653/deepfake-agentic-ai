64aeac4abd2b1889332becb2fa2e6f52f0cb1fdb
SANTHOSH P <santhosh11042007@gmail.com>
2026-04-02 10:21:27 +0530

    Update Dockerfile.ml

--- Dockerfile.ml
+++ Dockerfile.ml
@@ -1,13 +1,13 @@
 FROM python:3.11-slim
 
-# Install runtime dependencies (ffmpeg and OpenGL libs)
-#RUN apt-get update && \
- #   apt-get install -y --no-install-recommends ffmpeg libgl1 libglib2.0-0 && \
-  #  rm -rf /var/lib/apt/lists/*
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends ffmpeg libgl1 libglib2.0-0 && \
+    rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
 
 COPY ml/requirements.txt .
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY ml ./ml
