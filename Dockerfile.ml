c74c3704f11984c78a2e53cb603c25a3a654b784
SANTHOSH P <santhosh11042007@gmail.com>
2026-04-25 08:32:37 +0530

    Update Dockerfile.ml

--- Dockerfile.ml
+++ Dockerfile.ml
@@ -1,11 +1,7 @@
 FROM python:3.11-slim
-
 WORKDIR /app
-
 COPY ml/requirements.txt .
-
 RUN pip install --no-cache-dir -r requirements.txt
-
 COPY ml ./ml
-
+COPY shared/ ./shared/
 CMD ["python", "ml/inference.py"]
